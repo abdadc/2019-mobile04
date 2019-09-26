79221e386badef18670c2a889a3034f1113a9c94
abdadc <auri.culare@yahoo.com>
2019-09-26 07:32:35 +0700

    Mempelajari dan membuat intent (Implicit, Explicit, Bundle, Parcelable

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,8 +3,17 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static id.ac.polinema.intent.BundleActivity.AGE_KEY;
+import static id.ac.polinema.intent.BundleActivity.NAME_KEY;
+import static id.ac.polinema.intent.BundleActivity.USERNAME_KEY;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +21,21 @@ public class ProfileBundleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_bundle);
 
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             // TODO: display value here
+            String username = extras.getString(USERNAME_KEY);
+            usernameText.setText(username);
+
+            String name = extras.getString(NAME_KEY);
+            nameText.setText(name);
+
+            String age = String.valueOf(extras.getInt(AGE_KEY));
+            ageText.setText(age);
         }
     }
 }
